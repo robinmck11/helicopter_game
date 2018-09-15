fd25420ca4c1c933cea7500fe3ffc9723f9d51ad
tech-a44 <robin.mck11@hotmail.co.uk>
2018-09-15 12:19:09 +0100

    Updated Helicopter Object and created board object

--- js/app.js
+++ js/app.js
@@ -1,35 +1,56 @@
-/*
-  Helicopter
-    - Attributes
-      - xPos
-      - yPos
-    - Behaviours
-      - move down
-      - move up
-*/
+$(document).ready(function() {
 
-var helicopter = {
+  /*
+    Board
+      - Attributes
+        - width
+        -height
+        ...
+      - Behaviours
+        - ...
+  */
 
-  // start at half height
-  xPos: 100,
-  yPos: $("#container").height(),
+  var board = {
+    width: $("#container").width(),
+    height: $("#container").height(),
+  }
 
-  getxPos: function(){
-    return xPos;
-  },
 
-  getyPos: function(){
-    return this.yPos;
+    /*
+      Helicopter
+        - Attributes
+          - xPos
+          - yPos
+          - width
+          -height
+          ...
+        - Behaviours
+          - move down
+          - move up
+    */
 
-  },
+    var helicopter = {
 
-  setxPos: function(){
+      // start at half board height
+      xPos: 50,
+      yPos: board.height / 2,
 
-  },
+      // Future proofing
 
-  setyPos: function(){
+      setxPos: function() {
+        xPos+=1;
+      },
 
-  },
-}
+      setyPos: function() {
+        yPos+=1;
+      },
+    }
 
-console.log(helicopter.getyPos());
+    console.log(board.width);
+    console.log(board.height);
+
+    console.log("Helicopter xposition: " + helicopter.xPos);
+    console.log("Helicopter yposition: " + helicopter.yPos);
+
+
+});
